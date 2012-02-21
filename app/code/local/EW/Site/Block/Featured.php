15ab04c30c3278e765319f19b30db42356ea4d69
AJ Tarachanowicz II <aj@tarachanowicz.com>
2012-02-21 05:44:35 -0500

    added EW_Site module & featured block

---
+++ app/code/local/EW/Site/Block/Featured.php
@@ -0,0 +1,27 @@
+<?php
+
+class EW_Site_Block_Featured extends Mage_Core_Block_Template
+{
+
+    public function getFeatured()
+    {
+        $todayDate  = Mage::app()->getLocale()->date()->toString(Varien_Date::DATETIME_INTERNAL_FORMAT);
+
+        $category_id = 6;
+        $products = Mage::getModel('catalog/category')->load($category_id)
+            ->getProductCollection()
+            ->addAttributeToSelect('*')
+            ->addAttributeToFilter('status', 1)
+            ->addAttributeToFilter('news_from_date', array('date' => true, 'to' => $todayDate))
+            ->addAttributeToFilter('news_to_date', array('or'=> array(
+                0 => array('date' => true, 'from' => $todayDate),
+                1 => array('is' => new Zend_Db_Expr('null')))
+            ), 'left')
+            ->addAttributeToSort('news_from_date', 'desc')
+            ->setPageSize(3);
+
+        return $products; 
+    }
+}
+
+?>
