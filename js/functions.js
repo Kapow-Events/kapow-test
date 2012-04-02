fb196d0111f4c47bb8b4717799856033128fb413
Tim Novinger <tim.novinger@gmail.com>
2012-04-02 17:32:04 -0500

    Chore: Reduced height of slidebox

--- js/functions.js
+++ js/functions.js
@@ -82,7 +82,7 @@ var cj =
         $slidebox     = jQuery('#slidebox'),
         $content      = $slidebox.find('.content'),
         $close_button = $slidebox.find('#slidebox_close'),
-        height        = self.$window.height() >= 800 ? 600 : self.$window.height() - 200;
+        height        = self.$window.height() >= 800 ? 520 : self.$window.height() - 270;
 
     // Setup an alias
     self.$slidebox = $slidebox;
