8ab1d6ef9640d94bb2690d4490b0820a6309c2fb
Alexander J. Tarachanowicz II <aj@tarachanowicz.com>
2012-04-06 20:02:56 +0000

    Feature: Added redirect to homepage with thank-you GET var for newly registered users

---
+++ app/code/local/EW/Customer/controllers/AccountController.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Customer
+ * @copyright   Copyright (c) 2011 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Customer account controller
+ *
+ * @category   Mage
+ * @package    Mage_Customer
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+
+require_once 'Mage/Customer/controllers/AccountController.php';
+
+class EW_Customer_AccountController extends Mage_Customer_AccountController
+{
+
+    /**
+     * Add welcome message and send new account email.
+     * Returns success URL
+     *
+     * @param Mage_Customer_Model_Customer $customer
+     * @param bool $isJustConfirmed
+     * @return string
+     */
+    protected function _welcomeCustomer(Mage_Customer_Model_Customer $customer, $isJustConfirmed = false)
+    {
+        $this->_getSession()->addSuccess(
+            $this->__('Thank you for registering with %s.', Mage::app()->getStore()->getFrontendName())
+        );
+
+        $customer->sendNewAccountEmail(
+            $isJustConfirmed ? 'confirmed' : 'registered',
+            '',
+            Mage::app()->getStore()->getId()
+        );
+
+        $successUrl = Mage::getUrl('*/*/index', array('_secure'=>true));
+        if ($this->_getSession()->getBeforeAuthUrl()) {
+            $successUrl = $this->_getSession()->getBeforeAuthUrl(true);
+        }
+
+        $successUrl = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_LINK) .'?thank-you';
+
+        return $successUrl;
+    }
+
+}
