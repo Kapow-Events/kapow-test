f60a6aa66b53c35072dfff98b5dcf37caab8de9b
Alexander J. Tarachanowicz II <aj@colorjar.com>
2012-10-18 10:54:40 -0400

    Added wordpress resources

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'eventswi_kapow');
+define('DB_NAME', 'eventswi_stage');
 
 /** MySQL database username */
 define('DB_USER', 'eventswi_magento');
