cc39eb8436dc740ee493e37d6a5b89b70d6f2bbd
Brett Gilbert <brettgil@gmail.com>
2012-11-15 18:08:59 -0600

    added two google tracking scripts

--- app/code/local/EW/Customer/controllers/AccountController.php
+++ app/code/local/EW/Customer/controllers/AccountController.php
@@ -48,7 +48,42 @@ class EW_Customer_AccountController extends Mage_Customer_AccountController
     protected function _welcomeCustomer(Mage_Customer_Model_Customer $customer, $isJustConfirmed = false)
     {
         $this->_getSession()->addSuccess(
-            $this->__('Thank you for registering with %s.', Mage::app()->getStore()->getFrontendName())
+            $this->__('<!-- Google Code for New Account Signup Conversion Page -->
+            
+            <script type="text/javascript">
+            
+            /* <![CDATA[ */
+            
+            var google_conversion_id = 1003217271;
+            
+            var google_conversion_language = "en";
+            
+            var google_conversion_format = "3";
+            
+            var google_conversion_color = "ffffff";
+            
+            var google_conversion_label = "c5TTCImU3wUQ98Kv3gM";
+            
+            var google_conversion_value = 0;
+            
+            /* ]]> */
+            
+            </script>
+            
+            <script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion.js">
+            
+            </script>
+            
+            <noscript>
+            
+            <div style="display:inline;">
+            
+            <img height="1" width="1" style="border-style:none;" alt="" src="http://www.googleadservices.com/pagead/conversion/1003217271/?value=0&amp;label=c5TTCImU3wUQ98Kv3gM&amp;guid=ON&amp;script=0"/>
+            
+            </div>
+            
+            </noscript>
+            Thank you for registering with %s.', Mage::app()->getStore()->getFrontendName())
         );
 
         $customer->sendNewAccountEmail(
