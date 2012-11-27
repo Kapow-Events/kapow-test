9ef45131302860c6867f630933cdc8b73b3645e0
Alexander J. Tarachanowicz II <aj@tarachanowicz.com>
2012-11-27 09:55:10 -0500

    Updated the email sender & address to use the custom email address form the config settings

--- app/code/local/EW/Contacts/controllers/RequestController.php
+++ app/code/local/EW/Contacts/controllers/RequestController.php
@@ -34,19 +34,9 @@
 class EW_Contacts_RequestController extends Mage_Core_Controller_Front_Action
 {
 
-    const XML_PATH_EMAIL_RECIPIENT  = 'contacts/email/recipient_email';
-    const XML_PATH_EMAIL_SENDER     = 'contacts/email/sender_email_identity';
+    const XML_PATH_EMAIL_RECIPIENT  = 'trans_email/ident_custom2/email';
+    const XML_PATH_EMAIL_SENDER     = 'trans_email/ident_custom2/name';
     const XML_PATH_EMAIL_TEMPLATE   = 'contacts/email/email_template';
-    const XML_PATH_ENABLED          = 'contacts/contacts/enabled';
-
-    public function preDispatch()
-    {
-        parent::preDispatch();
-
-        if( !Mage::getStoreConfigFlag(self::XML_PATH_ENABLED) ) {
-            $this->norouteAction();
-        }
-    }
 
     public function indexAction()
     {
